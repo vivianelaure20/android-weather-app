8a46339246f9076a27df0842aaba63eb3c2fc18a
Felly Mwepu <fellymwepu@4-sure.net>
2022-02-07 21:02:36 +0200

    - get coordinates when clicking on the map

--- app/src/main/java/com/example/a4sure_weather_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/a4sure_weather_app/ui/MainActivity.kt
@@ -1,34 +1,28 @@
-package com.example.a4sure_weather_app
+package com.example.a4sure_weather_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.a4sure_weather_app.R
 import com.example.a4sure_weather_app.databinding.ActivityMainBinding
-import com.example.a4sure_weather_app.ui.MapFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Inflate the layout for this fragment
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(MapFragment())
     }
 
-
     private fun replaceFragment(fragment: Fragment) {
-
         val fragmentManager = supportFragmentManager
         val fragmentTransaction =  fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragmentContainer, fragment)
         fragmentTransaction.commit()
-
     }
 
 }
